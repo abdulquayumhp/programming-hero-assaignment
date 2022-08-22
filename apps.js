b22274b360c36b81a6b30522d95f741f4d68ae8a
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-08-22 20:04:44 +0600

    footer add

--- apps.js
+++ apps.js
@@ -1,3 +1,9 @@
+
+
+// input value golbaly function
+// input value golbaly function
+
+
 function getInputValue(inputValue){
 
     const inputValueField = inputValue.value;
@@ -5,13 +11,19 @@ function getInputValue(inputValue){
     return inputValueFieldString;
 
 }
+
+// addad player for multipul fuction 
 const perPlayerCount = document.getElementById("Total-added-player")
 
 
-
-
-
+// all button for array with parantNode accecs multipul funcion 
 const cartArray = [];
+
+
+
+// palyer add and number count how much player add && select player
+// palyer add and number count how much player add && select player
+
 
 function display(cartproduct){
 
@@ -20,61 +32,66 @@ function display(cartproduct){
 
     for(let i = 0; i < cartproduct.length; i++){
        const numberOfPlayer = i;
-       console.log(numberOfPlayer)
-       
-    if(numberOfPlayer < 5 ){
-        const nameBox = cartArray[i].PlayerName;
-   
-        const tr = document.createElement("tr");
 
+
+    //    if condition for extra no more player add 
+    //    if condition for extra no more player add 
+
+
+
+    if(numberOfPlayer < 5 ){
+
+        const nameBox = cartArray[i].PlayerName;
+        const tr = document.createElement("tr");
         tr.innerHTML=`
         <th>${i+1}</th>
         <td>${nameBox}</td>
         
         `;
-
         tableBody.appendChild(tr);
       
     }else{
         alert("please you can't take no more player")
         return numberOfPlayer;
-       
-
+    
     }
+
+        // calculate player indivisual and per palyer cost multipication 
+        // calculate player indivisual and per palyer cost multipication 
+
     document.getElementById("calculate-btn").addEventListener("click",function(){
+
         const addedPlayer = numberOfPlayer+1;
-        console.log(addedPlayer)
-    
         const perPlayeroutput = getInputValue(perPlayerCount);
-    
         const playerExpenses = addedPlayer * perPlayeroutput;
-        console.log(playerExpenses)
-    
-    
         const playerCalculate = document.getElementById("player-expanses");
-    
-    
         playerCalculate.innerText = playerExpenses;
     
     });
-    
-
+   
    }
 
-
-
-
 }
+
+
+
+// all button with one css class by quarySelectorAll
+// all button with one css class by quarySelectorAll
+// all button with one css class by quarySelectorAll
 
 const selectAllButton = document.querySelectorAll('.item');
 for (const select of selectAllButton) {
     select.addEventListener('click', function (event) {
-       
+
+        // button disable 
         event.target.disabled = true;
         event.target.style.background = "gray";
+
+
+
         const name = event.target.parentNode.children[0].innerText;
         
-
+        // button and name value as a object 
         const PlayerObj = {
             PlayerName : name,
             
@@ -82,20 +99,16 @@ for (const select of selectAllButton) {
     
         cartArray.push(PlayerObj);
     
-    
+
         display(cartArray);
 
-
-        
-
-        
-        
 
     })
 }
 
 
-
+// univasal function for all scope function
+// get value by call another function  
 
 function getInputValue(inputValue){
 
@@ -107,26 +120,23 @@ function getInputValue(inputValue){
 
 
 
-
+// calculate all sub total 
+// calculate all sub total 
+// calculate all sub total 
 
 
 
 document.getElementById("total-Calculate").addEventListener("click",function(){
+
+
         const managerExpanse = document.getElementById("manager-expanse");
-
         const coachExpanse = document.getElementById("coach-expanse");
-
-
-
-
         const  managerOutput = getInputValue(managerExpanse);
-
-
-
         const coachOutput = getInputValue(coachExpanse);
 
-        const  playerCalculateTwo = document.getElementById("player-expanses").innerText;
 
+
+        const  playerCalculateTwo = document.getElementById("player-expanses").innerText;
         const playerCalculateStrig =parseInt(playerCalculateTwo) 
 
 
@@ -134,19 +144,11 @@ document.getElementById("total-Calculate").addEventListener("click",function(){
         const perManagerOptput =  managerOutput + coachOutput + playerCalculateStrig ;
 
 
-
-
         const totalTeamExpanse = document.getElementById("total");
-
-
         totalTeamExpanse.innerText = perManagerOptput;
-
 
         managerExpanse.value="";
         coachExpanse.value="";
-
-
-
 
 
 }); 
